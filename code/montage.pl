ce20a832b5d1b05bf276c907baf87319dbc2b56c
Pavel Gonchar <pavel.gonchar@gmail.com>
2016-09-15 21:29:00 +0300

    Update montage.pl

--- code/montage.pl
+++ code/montage.pl
@@ -17,7 +17,8 @@ while (my $line = <>) {
 my $imagelist = join " ", @images;
 my $rnd = rand();
 `montage -label %f -size 896x1120 -tile 4x5 -coalesce -quality 100 -geometry 224x224+0+0 $imagelist /tmp/montage-$rnd.jpg`;
-`aws s3 cp /tmp/montage-$rnd.jpg s3://pg-image-search/montages/ --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers`;
+#`aws s3 cp /tmp/montage-$rnd.jpg s3://pg-image-search/montages/ --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers`;
 
-print "https://s3-us-west-2.amazonaws.com/pg-image-search/montages/montage-$rnd.jpg\n";
+#print "https://s3-us-west-2.amazonaws.com/pg-image-search/montages/montage-$rnd.jpg\n";
+print "/tmp/montage-$rnd.jpg\n";
 exit;
